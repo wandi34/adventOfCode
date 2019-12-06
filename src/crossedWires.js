be3df12eadcee5155f7cd25fdd80f25324a350da
Andreas Winter <awi@adorsys.de>
2019-12-06 08:56:32 +0100

    Refactor solution for day3

--- src/crossedWires.js
+++ src/crossedWires.js
@@ -3,15 +3,13 @@ exports.__esModule = true;
 function crossedWires(wire1, wire2) {
     var cursorList_w1 = iterateWire(wire1);
     var cursorList_w2 = iterateWire(wire2);
-    var crossedCursors = Array.from(getSameElements(cursorList_w1, cursorList_w2));
+    var crossedCursors = Array.from(getSameElements(new Set(cursorList_w1), new Set(cursorList_w2)));
     var distances = crossedCursors.map(function (c) { return Math.abs(Number(c.split('|')[0])) + Math.abs(Number(c.split('|')[1])); });
     return distances.sort(function (a, b) { return a - b; })[0];
 }
 exports.crossedWires = crossedWires;
-function getSameElements(cursorList1, cursorList2) {
+function getSameElements(cursorSet1, cursorSet2) {
     var result = new Set();
-    var cursorSet1 = new Set(cursorList1);
-    var cursorSet2 = new Set(cursorList2);
     cursorSet1.forEach(function (cursor) {
         if (cursorSet2.has(cursor) && cursor !== "0|0") {
             result.add(cursor);
@@ -24,55 +22,29 @@ function iterateWire(wire) {
     wire.map(function (rule) {
         var inst = rule.substring(0, 1);
         var steps = Number(rule.substring(1));
-        if (inst === "R") {
-            cursorList = rightAdd(steps, cursorList);
-        }
-        else if (inst === "L") {
-            cursorList = leftAdd(steps, cursorList);
-        }
-        else if (inst === "U") {
-            cursorList = upAdd(steps, cursorList);
-        }
-        else if (inst === "D") {
-            cursorList = downAdd(steps, cursorList);
-        }
+        cursorList = calculate(steps, inst, cursorList);
     });
     return cursorList;
 }
-function addToString(string, input) {
-    var stringAsNumber = string.split('|');
-    var inputAsNumber = input.split('|');
-    return Number(stringAsNumber[0]) + Number(inputAsNumber[0]) + "|" + (Number(stringAsNumber[1]) + Number(inputAsNumber[1]));
+function calcNewCursor(cursor, addition) {
+    var cursorSplitted = cursor.split('|');
+    var additionSplittet = addition.split('|');
+    return Number(cursorSplitted[0]) + Number(additionSplittet[0]) + "|" + (Number(cursorSplitted[1]) + Number(additionSplittet[1]));
 }
-function rightAdd(steps, cursorList) {
+function calculate(steps, inst, cursorList) {
     while (steps > 0) {
         var lastCursor = cursorList[cursorList.length - 1];
-        cursorList.push(addToString(lastCursor, "1|0"));
+        cursorList.push(calcNewCursor(lastCursor, getCalculation(inst)));
         --steps;
     }
     return cursorList;
 }
-function leftAdd(steps, cursorList) {
-    while (steps > 0) {
-        var lastCursor = cursorList[cursorList.length - 1];
-        cursorList.push(addToString(lastCursor, "-1|0"));
-        --steps;
+function getCalculation(instruction) {
+    switch (instruction) {
+        case "R": return "1|0";
+        case "L": return "-1|0";
+        case "U": return "0|1";
+        case "D": return "0|-1";
+        default: throw new Error("Given instruction " + instruction + " is not valid.");
     }
-    return cursorList;
-}
-function upAdd(steps, cursorList) {
-    while (steps > 0) {
-        var lastCursor = cursorList[cursorList.length - 1];
-        cursorList.push(addToString(lastCursor, "0|1"));
-        --steps;
-    }
-    return cursorList;
-}
-function downAdd(steps, cursorList) {
-    while (steps > 0) {
-        var lastCursor = cursorList[cursorList.length - 1];
-        cursorList.push(addToString(lastCursor, "0|-1"));
-        --steps;
-    }
-    return cursorList;
 }
--- src/crossedWires.ts
+++ src/crossedWires.ts
@@ -1,16 +1,14 @@
 export function crossedWires(wire1: string[], wire2: string[]): number {
-  const cursorList_w1: String[] = iterateWire(wire1)
-  const cursorList_w2: String[] = iterateWire(wire2)
+  const cursorList_w1: string[] = iterateWire(wire1)
+  const cursorList_w2: string[] = iterateWire(wire2)
 
-  const crossedCursors: String[] = Array.from(getSameElements(cursorList_w1, cursorList_w2))
+  const crossedCursors: string[] = Array.from(getSameElements(new Set(cursorList_w1), new Set(cursorList_w2)))
   const distances: number[] = crossedCursors.map(c => Math.abs(Number(c.split('|')[0])) + Math.abs(Number(c.split('|')[1])))
   return distances.sort((a, b) => a - b)[0]
 }
 
-function getSameElements(cursorList1: String[], cursorList2: String[]): Set<String> {
-  let result: Set<String> = new Set()
-  const cursorSet1 = new Set(cursorList1)
-  const cursorSet2 = new Set(cursorList2)
+function getSameElements(cursorSet1: Set<string>, cursorSet2: Set<string>): Set<string> {
+  let result: Set<string> = new Set()
   cursorSet1.forEach(cursor => {
     if (cursorSet2.has(cursor) && cursor !== "0|0") {
           result.add(cursor)
@@ -24,57 +22,32 @@ function iterateWire(wire: string[]): string[] {
   wire.map(rule => {
     const inst = rule.substring(0, 1);
     const steps = Number(rule.substring(1));
-    if (inst === "R") {
-      cursorList = rightAdd(steps, cursorList)
-    } else if (inst === "L") {
-      cursorList = leftAdd(steps, cursorList)
-    } else if (inst === "U") {
-      cursorList = upAdd(steps, cursorList)
-    } else if (inst === "D") {
-      cursorList = downAdd(steps, cursorList)
-    }
+    cursorList = calculate(steps, inst, cursorList);
   });
   return cursorList
 }
 
-function addToString(string: string, input: string): string {
-  const stringAsNumber: string[] = string.split('|')
-  const inputAsNumber: string[] = input.split('|')
-  return `${Number(stringAsNumber[0]) + Number(inputAsNumber[0])}|${Number(stringAsNumber[1]) + Number(inputAsNumber[1])}`
+function calcNewCursor(cursor: string, addition: string): string {
+  const cursorSplitted: string[] = cursor.split('|')
+  const additionSplittet: string[] = addition.split('|')
+  return `${Number(cursorSplitted[0]) + Number(additionSplittet[0])}|${Number(cursorSplitted[1]) + Number(additionSplittet[1])}`
 }
 
-function rightAdd(steps: number, cursorList: string[]): string[] {
+function calculate(steps: number, inst: string, cursorList: string[]): string[] {
   while (steps > 0) {
     const lastCursor = cursorList[cursorList.length - 1];
-    cursorList.push(addToString(lastCursor, "1|0"))
+    cursorList.push(calcNewCursor(lastCursor, getCalculation(inst)))
     --steps
   }
   return cursorList
 }
 
-function leftAdd(steps: number, cursorList: string[]): string[] {
-  while (steps > 0) {
-    const lastCursor = cursorList[cursorList.length - 1];
-    cursorList.push(addToString(lastCursor, "-1|0"))
-    --steps
+function getCalculation(instruction: string): string {
+  switch (instruction) {
+    case "R": return "1|0"
+    case "L": return "-1|0"
+    case "U": return "0|1"
+    case "D": return "0|-1"
+    default: throw new Error(`Given instruction ${instruction} is not valid.`)
   }
-  return cursorList
-}
-
-function upAdd(steps: number, cursorList: string[]): string[] {
-  while (steps > 0) {
-    const lastCursor = cursorList[cursorList.length - 1];
-    cursorList.push(addToString(lastCursor, "0|1"))
-    --steps
-  }
-  return cursorList
-}
-
-function downAdd(steps: number, cursorList: string[]): string[] {
-  while (steps > 0) {
-    const lastCursor = cursorList[cursorList.length - 1];
-    cursorList.push(addToString(lastCursor, "0|-1"))
-    --steps
-  }
-  return cursorList
 }
